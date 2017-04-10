624fd1fe58960c710177cfcba62c28aaa3a9c69e
Eugene Smith <easmith@mail.ru>
2017-04-10 13:38:26 +0300

    semaphore

--- apr07/w02/src/ru/easmith/Consumer.java
+++ apr07/w02/src/ru/easmith/Consumer.java
@@ -9,28 +9,33 @@ import java.util.Stack;
  */
 public class Consumer {
 
-    private Stack<Integer> cubator = new Stack<>();
-    private Stack<Integer> kvadrator = new Stack<>();
-    private Stack<Integer> simplator = new Stack<>();
+    private Object cubator = new Object();
+    private Object kvadrator = new Object();
+    private Object simplator = new Object();
 
     public void sum(int resCubator, int resKvadrator, int resSimplator) {
         System.out.println("invoke " + resCubator + " " + resKvadrator + " " + resSimplator);
-        synchronized (this) {
-            if (resCubator > 0) {
-                cubator.push(resCubator);
-            }
-            if (resKvadrator > 0) {
-                kvadrator.push(resKvadrator);
-            }
-            if (resSimplator > 0) {
-                simplator.push(resSimplator);
-            }
-            while (!cubator.empty() && !kvadrator.empty() && !simplator.empty()) {
-                int kva = kvadrator.pop();
-                int cub = cubator.pop();
-                int sim = simplator.pop();
-                System.out.println("ConsumerSum: " + cub + " + " + kva + " + " + sim + " = " + (cub + kva + sim));
+
+        int result = 0;
+
+        if (resCubator > 0) {
+            synchronized (cubator) {
+                result += resCubator;
             }
         }
+
+        if (resKvadrator > 0) {
+            synchronized (kvadrator) {
+                result += resKvadrator;
+            }
+        }
+
+        if (resSimplator > 0) {
+            synchronized (simplator) {
+                result += resSimplator;
+            }
+        }
+
+        System.out.println(result);
     }
 }
